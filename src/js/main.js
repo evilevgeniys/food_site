6d8156ed6acf02cb850ce3554a7ead8cfb032d2a
evilevgeniys <903jenya@gmail.com>
2023-11-30 20:41:46 +0300

    refactor modalFunc with FetchAPI

--- src/js/main.js
+++ src/js/main.js
@@ -229,10 +229,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
      //post forms
 
+
      const forms = document.querySelectorAll('form');
 
      const message = {
-        loading: 'Загрузка',
+        loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с Вами свяжемся',
         failure: 'Что-то пошло не так...'
      };
@@ -245,16 +246,16 @@ window.addEventListener('DOMContentLoaded', () => {
         form.addEventListener('submit', (e) => {
             e.preventDefault(); // самая первая команда, которая должна быть написана при AJAX запросах!!!!
             
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
 
-            form.appendChild(statusMessage);
+            // Лучше добавлять css стили напрямую в css file 
+            statusMessage.style.cssText = `    
+                display: block;
+                margin: 0 auto;
+            `;
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
+            form.insertAdjacentElement('afterend', statusMessage);
 
             const formData = new FormData(form);
 
@@ -264,23 +265,25 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if(request.status === 200){
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            }) 
-
-
-
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure);
+            })
+            .finally(() => {
+                form.reset();
+            })
         })
      }
 
@@ -310,6 +313,19 @@ window.addEventListener('DOMContentLoaded', () => {
             prevModalDialog.classList.add('show');
             prevModalDialog.classList.remove('hide');
             closeModal();
-        }, 600000);
+        }, 4000);
      }
+    
+
+     //Fetch API lesson
+
+     fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({name: 'Alex'}),
+        headers: {
+            'Content-type': 'application/json'
+        }
+     })
+      .then(response => response.json())
+      .then(json => console.log(json))
 })
