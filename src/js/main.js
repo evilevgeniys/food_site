69a838cbd2638c2d423ff65bf4e9e017f624dc6c
evilevgeniys <903jenya@gmail.com>
2023-12-07 21:04:41 +0300

    added calculator

--- src/js/main.js
+++ src/js/main.js
@@ -334,49 +334,198 @@ window.addEventListener('DOMContentLoaded', () => {
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           total = document.querySelector('#total'),
-          current = document.querySelector('#current');
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
 
     let slideIndex = 1;
-
-    showSlides(slideIndex);
+    let offset = 0;
 
     if(slides.length < 10){
         total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
     } else {
         total.textContent = slides.length;
+        current.textContent = slideIndex;
     }
 
-    function showSlides(index){
-        
-        if(index > slides.length){
-            slideIndex = 1;
-        }
 
-        if (index < 1){
-            slideIndex = slides.length;
-        }
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
 
-        slides.forEach(item => item.style.display = 'none');
+    //Создаем карусель
 
-        slides[slideIndex - 1].style.display = 'block';
+    slidesField.style.width = 100 * slides.length + '%'; //Для того, чтобы поместить все слайды на страницу
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
 
-        if(slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
+    slidesWrapper.style.overflow = 'hidden'
+
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener('click', () => {
+        if(offset == deleteNotDigits(width) * (slides.length - 1)){
+            offset = 0;
         } else {
-            current.textContent = slideIndex;
+            offset += deleteNotDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if(slideIndex == slides.length){
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if(slides.length < 10){
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
+    })
+
+    prev.addEventListener('click', () => {
+        if(offset == 0){
+            offset = deleteNotDigits(width) * (slides.length - 1)
+        } else {
+            offset -= deleteNotDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if(slideIndex == 1){
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if(slides.length < 10){
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }        
+    })
+
+
+
+
+    // showSlides(slideIndex);
+
+    // if(slides.length < 10){
+    //     total.textContent = `0${slides.length}`;
+    // } else {
+    //     total.textContent = slides.length;
+    // }
+
+    // function showSlides(index){
+        
+    //     if(index > slides.length){
+    //         slideIndex = 1;
+    //     }
+
+    //     if (index < 1){
+    //         slideIdex = slides.length;
+    //     }
+
+    //     slides.forEach(item => item.style.display = 'none');
+
+    //     slides[slideIndex - 1].style.display = 'block';
+
+    //     if(slides.length < 10) {
+    //         current.textContent = `0${slideIndex}`;
+    //     } else {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+
+    // function plusSlides(n){
+    //     showSlides(slideIndex += n);
+    // }
+
+    // prev.addEventListener('click', function(){
+    //     plusSlides(-1);
+    // })
+
+    // next.addEventListener('click', function() {
+    //     plusSlides(1);
+    // })
+
+    //создаем "Калькулятор"
+
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    function calcTotal(){
+        if(!sex || !height || !weight || !age || !ratio){
+            result.textContent = '____';
+            return;
+        } 
+
+        if(sex === 'female'){
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio)
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio)
         }
     }
 
-    function plusSlides(n){
-        showSlides(slideIndex += n);
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass){
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        document.querySelector(parentSelector).addEventListener('click', (e) => {
+            if(e.target.getAttribute('data-ratio')){
+                ratio = +e.target.getAttribute('data-ratio'); 
+            } else {
+                sex = e.target.getAttribute('id');
+            }
+
+            console.log(ratio, sex);
+            
+            elements.forEach(elem => {
+                elem.classList.remove(activeClass);
+            });
+            
+            e.target.classList.add(activeClass);
+            calcTotal();
+        })
+
     }
 
-    prev.addEventListener('click', function(){
-        plusSlides(-1);
-    })
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
 
-    next.addEventListener('click', function() {
-        plusSlides(1);
-    })
+
+    function getDynamicInformation(selector){
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')){
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+                
+                }
+            calcTotal();
+
+        });
+    }
+    
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+    
     
 })
