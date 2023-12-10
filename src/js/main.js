841f0fb06189a021c06fe2a2e407f10b9c927735
evilevgeniys <903jenya@gmail.com>
2023-12-10 23:56:01 +0300

    добавлены проверки на правильность введенных данных и работа c localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -459,9 +459,22 @@ window.addEventListener('DOMContentLoaded', () => {
     //создаем "Калькулятор"
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female', 
-        height, weight, age, 
-        ratio = 1.375;
+
+    let sex, height, weight, age, ratio;
+    
+    if(localStorage.getItem('sex')){
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female'
+        localStorage.setItem('sex', 'female');
+    }
+
+    if(localStorage.getItem('ratio')){
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375
+        localStorage.setItem('ratio', 1.375);
+    }
 
     function calcTotal(){
         if(!sex || !height || !weight || !age || !ratio){
@@ -485,8 +498,10 @@ window.addEventListener('DOMContentLoaded', () => {
             elem.addEventListener('click', (e) => {
                 if(e.target.getAttribute('data-ratio')){
                     ratio = +e.target.getAttribute('data-ratio'); 
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', +e.target.getAttribute('id'));
                 }
     
                 console.log(ratio, sex);
@@ -509,6 +524,14 @@ window.addEventListener('DOMContentLoaded', () => {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+            
+            if(input.value.match(/\D/g)){
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+                
+            }; 
+
             switch(input.getAttribute('id')){
                 case 'height':
                     height = +input.value;
