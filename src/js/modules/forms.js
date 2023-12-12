eb83084dffb71189e74f9951a5c44019288577be
evilevgeniys <903jenya@gmail.com>
2023-12-12 20:38:40 +0300

    some refatctor code in forms.js

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -57,6 +57,35 @@ function forms() {
             })
         })
      }
+
+     function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+
+        showModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('.modal__dialog');
+
+        thanksModal.innerHTML = `
+            <div class="modal__dialog">
+                <div class="modal__content">
+                    <div class="modal__close" data-close>×</div>
+                    <div class="modal__title">${message}</div>
+                </div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+     }
 }
 
 module.exports = forms;
