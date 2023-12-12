23b83af34027666208d7a58e6e21810e97a82fe5
evilevgeniys <903jenya@gmail.com>
2023-12-12 20:55:43 +0300

    clean some useless code

--- src/js/script.js
+++ src/js/script.js
@@ -19,12 +19,5 @@ window.addEventListener('DOMContentLoaded', () => {
      fetch('http://localhost:3000/menu')
         .then(data => data.json())
         .then(res => console.log(res));
-
-
-
-    //создаем "Калькулятор"
-
-    
-    
-    
+     
 })
