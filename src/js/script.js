aff730cae344d65714dc5039ed92f8f213786625
evilevgeniys <903jenya@gmail.com>
2023-12-12 21:32:29 +0300

    refactor script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,14 @@
 'use strict'
 
+import tabs  from './modules/tabs';
+import modal  from './modules/modal';
+import timer  from './modules/timer';
+import calc  from './modules/calc';
+import cards  from './modules/cards';
+import forms from './modules/forms';
+import slider  from './modules/slider';
+
 window.addEventListener('DOMContentLoaded', () => {
-    const tabs = require('./modules/tabs'),
-          modal = require('./modules/modal'),
-          timer = require('./modules/timer'),
-          calc = require('./modules/calc'),
-          cards = require('./modules/cards'),
-          forms = require('./modules/forms'),
-          slider = require('./modules/slider');
 
     tabs();
     modal();
